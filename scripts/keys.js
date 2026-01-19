e8193804282d6604e9b58dbc90dcd6552245f1f5
kekke-gk <gk0509hn257@gmail.com>
2026-01-19 16:50:21 +0900

    Remove unused API keys and related functions from the codebase

--- scripts/keys.js
+++
@@ -1,3 +0,0 @@
-export const NOTION_API_KEY = "1234567890abcdefghijklmnopqrstuvwxyz"
-export const NOTION_DATABASE_ID = "1234567890abcdefghijklmnopqrstuvwxyz"
-export const SEMANTIC_SCHOLAR_API_KEY = "1234567890abcdefghijklmnopqrstuvwxyz"
